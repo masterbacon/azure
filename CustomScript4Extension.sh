5cf50594ee9005ff34a6352cfab6ae7dde0c01ef
masterbacon <andrewwbacon@gmail.com>
2016-05-16 15:48:54 +0100

    Update CustomScript4Extension.sh

--- CustomScript4Extension.sh
+++ CustomScript4Extension.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
-yum -y install tomcat
-yum -y install tomcat-webapps
-yum -y install tomcat-admin-webapps
-
 firewall-cmd --permanent --add-port=8080/tcp
 firewall-cmd --permanent --add-service=http
 firewall-cmd --reload
 firewall-cmd --list-all
+
+yum -y install tomcat
+yum -y install tomcat-webapps
+yum -y install tomcat-admin-webapps
 
 systemctl restart tomcat
