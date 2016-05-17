75a5b0eebf94d50ec0eb7f0de0e163022f10ed05
masterbacon <andrewwbacon@gmail.com>
2016-05-17 16:53:48 +0100

    Added firewall cmds for secure HTTP access demo

--- DeployDocker.sh
+++ DeployDocker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -x
 
+# Set up firewall for certs/HTTPS demo that follows
+firewall-cmd --permanent --add-port=2376/tcp
+firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-service=https
+firewall-cmd --reload
+firewall-cmd --list-all
+
 yum -y update
 tee /etc/yum.repos.d/docker.repo <<-EOF
 [dockerrepo]
